2b52414f6e57f0a986806589c5b1a337dd169abd
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-08 01:15:50 +0700

    commit

--- order/migrations/0002_order_user.py
+++ order/migrations/0002_order_user.py
@@ -10,13 +10,13 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('cart', '0001_initial'),
+        ('order', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='cart',
+            model_name='order',
             name='user',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
