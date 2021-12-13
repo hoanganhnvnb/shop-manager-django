fc404f9fdb8d01fc6b9476a0477c02f8903ae4d3
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-13 23:49:26 +0700

    Add imageField to Category

--- category/models.py
+++ category/models.py
@@ -7,6 +7,7 @@ class Category(models.Model):
     slug = models.CharField(max_length=100, default=100)
     description = models.TextField(default='')
     active = models.BooleanField(default=True)
+    image = models.ImageField(null=True)
 
     def __str__(self):
         return str(self.title)
