86bba80777d9f8ccfd6d5abea7af37455ff771f5
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-19 21:56:53 +0700

    fix noti

--- FCMManager.py
+++ FCMManager.py
@@ -11,7 +11,7 @@ def sendPush(title, msg, registration_token, dataObject=None):
             body=msg
         ),
         data=dataObject,
-        token=registration_token,
+        tokens=registration_token,
     )
     
     response = messaging.send_multicast(message)
