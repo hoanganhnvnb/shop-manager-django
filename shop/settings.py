f6b92631a6b58d4a91f0278f673e9f54db0b1ca5
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-22 22:28:23 +0700

    change to sqlite

--- shop/settings.py
+++ shop/settings.py
@@ -104,23 +104,23 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {     
-    'default': {         
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',         
-        'NAME': 'shopnew',         
-        'USER': 'shopuser',         
-        'PASSWORD': 'password',         
-        'HOST': 'localhost',         
-        'PORT': '',     
-    } 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# DATABASES = {     
+#     'default': {         
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',         
+#         'NAME': 'shopnew',         
+#         'USER': 'shopuser',         
+#         'PASSWORD': 'password',         
+#         'HOST': 'localhost',         
+#         'PORT': '',     
+#     } 
+# }
 
 
 # Password validation
