796402c4aa28eb843a7d4d7d0c69af6d7a6bc1d3
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-22 16:18:39 +0700

    change db

--- shop/settings.py
+++ shop/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {     
     'default': {         
         'ENGINE': 'django.db.backends.postgresql_psycopg2',         
-        'NAME': 'shopdb',         
+        'NAME': 'shopnew',         
         'USER': 'shopuser',         
         'PASSWORD': 'password',         
         'HOST': 'localhost',         
@@ -147,7 +147,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-# TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
