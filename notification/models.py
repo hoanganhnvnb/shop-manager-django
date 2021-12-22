bf7862154ccdbd7a360314fad589fed455ac9d0e
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-23 01:26:44 +0700

    fix

--- notification/models.py
+++ notification/models.py
@@ -5,8 +5,8 @@ from user.models import CustomerUser
 
 
 class Notification(models.Model):
-    title = models.CharField(default='', max_length=255)
-    content = models.CharField(default='', max_length=500)
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=500)
     user = models.ForeignKey(CustomerUser, on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now=True)
     is_read = models.BooleanField(default=False)
