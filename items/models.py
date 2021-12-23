dd7d573b6ffe046e6edc6cd0b4e05c63a8f3228b
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-23 15:52:28 +0700

    barcode to BigIntegerField

--- items/models.py
+++ items/models.py
@@ -4,7 +4,7 @@ from category.models import Category
 # Create your models here.
 
 class Items(models.Model):
-    barcode = models.IntegerField(unique=True)
+    barcode = models.BigIntegerField(unique=True)
     title = models.CharField(max_length=255)
     description = models.TextField(default='')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
