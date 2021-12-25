156d65e4da63992c96f3cb2465d078a3f7892ff1
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-25 21:02:03 +0700

    decode base64 fix

--- search/views.py
+++ search/views.py
@@ -23,7 +23,7 @@ CharField.register_lookup(Lower)
 class GetItemsByTitle(APIView):
     def get(self, request, *args, **kwargs):
         search_text = kwargs.get('search_text')
-        search_text = base64.urlsafe_b64decode(search_text)
+        search_text = base64.b64decode(search_text)
         item_list = Items.objects.filter(title__unaccent__lower__trigram_similar=search_text)
         
         data = ItemsSerializers(item_list, many=True)
@@ -34,7 +34,7 @@ class GetItemsByTitle(APIView):
 class GetItemsByCompanyName(APIView):
     def get(self, request, *args, **kwargs):
         search_text = kwargs.get('search_text')
-        search_text = base64.urlsafe_b64decode(search_text)
+        search_text = base64.b64decode(search_text)
         item_list = Items.objects.filter(companyName__unaccent__lower__trigram_similar=search_text)
         data = ItemsSerializers(item_list, many=True)
         return Response(data=data.data, status=status.HTTP_200_OK)
@@ -43,7 +43,7 @@ class GetItemsByCategoryTitle(APIView):
     
     def get(self, request, *args, **kwargs):
         search_text = kwargs.get('search_text')
-        search_text = base64.urlsafe_b64decode(search_text)
+        search_text = base64.b64decode(search_text)
         item_list = Items.objects.filter(category__unaccent__lower__trigram_similar=search_text)
         data = ItemsSerializers(item_list, many=True)
         return Response(data=data.data, status=status.HTTP_200_OK)
