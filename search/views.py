7ddafcf1f97a675acd371563d312cc9ea0c95474
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-26 18:52:26 +0700

    convert vietnamese fix

--- search/views.py
+++ search/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from unidecode import unidecode
+import re
 
 from items.models import Items
 from items.serializers import ItemsSerializers
@@ -22,11 +23,28 @@ CharField.register_lookup(Lower)
 
 # Create your views here.
 
+def no_accent_vietnamese(s):
+    s = re.sub(r'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
+    s = re.sub(r'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
+    s = re.sub(r'[èéẹẻẽêềếệểễ]', 'e', s)
+    s = re.sub(r'[ÈÉẸẺẼÊỀẾỆỂỄ]', 'E', s)
+    s = re.sub(r'[òóọỏõôồốộổỗơờớợởỡ]', 'o', s)
+    s = re.sub(r'[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]', 'O', s)
+    s = re.sub(r'[ìíịỉĩ]', 'i', s)
+    s = re.sub(r'[ÌÍỊỈĨ]', 'I', s)
+    s = re.sub(r'[ùúụủũưừứựửữ]', 'u', s)
+    s = re.sub(r'[ƯỪỨỰỬỮÙÚỤỦŨ]', 'U', s)
+    s = re.sub(r'[ỳýỵỷỹ]', 'y', s)
+    s = re.sub(r'[ỲÝỴỶỸ]', 'Y', s)
+    s = re.sub(r'[Đ]', 'D', s)
+    s = re.sub(r'[đ]', 'd', s)
+    return s
+
 class GetItemsByTitle(APIView):
     def get(self, request, *args, **kwargs):
         search_text = kwargs.get('search_text')
         search_text = base64.b64decode(search_text)
-        search_text = str(unidecode(search_text))
+        search_text = no_accent_vietnamese(search_text)
         item_list = Items.objects.filter(title__unaccent__icontains=search_text)
         
         data = ItemsSerializers(item_list, many=True)
@@ -38,7 +56,7 @@ class GetItemsByCompanyName(APIView):
     def get(self, request, *args, **kwargs):
         search_text = kwargs.get('search_text')
         search_text = base64.b64decode(search_text)
-        search_text = str(unidecode(search_text))
+        search_text = no_accent_vietnamese(search_text)
         item_list = Items.objects.filter(companyName__unaccent__icontains=search_text)
         data = ItemsSerializers(item_list, many=True)
         return Response(data=data.data, status=status.HTTP_200_OK)
@@ -48,7 +66,7 @@ class GetItemsByCategoryTitle(APIView):
     def get(self, request, *args, **kwargs):
         search_text = kwargs.get('search_text')
         search_text = base64.b64decode(search_text)
-        search_text = str(unidecode(search_text))
+        search_text = no_accent_vietnamese(search_text)
         item_list = Items.objects.filter(category__title__unaccent__icontains=search_text)
         data = ItemsSerializers(item_list, many=True)
         return Response(data=data.data, status=status.HTTP_200_OK)
