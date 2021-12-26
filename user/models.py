bd32db09ef16c9de7a160eb8483713cac3f26fee
hoanganhnvnb <71348563+hoanganhnvnb@users.noreply.github.com>
2021-12-26 17:11:09 +0700

    fix user import

--- user/models.py
+++ user/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-
-from notification.models import Notification
 class CustomerUser(AbstractUser):
     phone_number = models.CharField(default='', max_length=12)
     address = models.CharField(default='', max_length=255)
